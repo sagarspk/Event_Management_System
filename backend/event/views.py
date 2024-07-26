efad1c1a3520758575c5fb4ff26d207768bd6813
sagarsapkota <sagar.191336@ncit.edu.np>
2024-07-26 23:36:48 +0545

    image upload function created

--- backend/event/views.py
+++ backend/event/views.py
@@ -15,3 +15,11 @@ class AddEvent(APIView):
             return Response('Event added successfully', status = status.HTTP_200_OK)
         else:
             return Response('Event creation failed', status=status.HTTP_400_BAD_REQUEST)
+
+
+class UploadImage(APIView):
+    def post(self,request):
+        data = request.data['image']
+        event = Event.objects.get(id=id)
+        event.image=data
+        return Response('Image updated successfully', status= status.HTTP_200_OK)

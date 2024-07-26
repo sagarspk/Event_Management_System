9b4369cb6869d5d15be86effdc849eaf11a2c3c5
sagarsapkota <sagar.191336@ncit.edu.np>
2024-07-26 23:35:30 +0545

    base url updated for image uploading

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import staticurl
 from django.urls import path,include
 
 urlpatterns = [
@@ -23,3 +25,5 @@ urlpatterns = [
     path('',include('accounts.urls')),
     path('',include('order.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
